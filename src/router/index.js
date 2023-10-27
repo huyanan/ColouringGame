61f63445e4b7ffb183d87caa7b84c3c4f7221f48
huyanan <huyanan@jiandanxinli.com>
2023-10-27 19:06:00 +0800

    feat: 游戏制作工具

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import game from '@/views/game'
+import gameMaker from '@/views/gameMaker'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'game',
       component: game
+    },
+    {
+      path: '/game_maker',
+      name: 'gameMaker',
+      component: gameMaker
     }
   ]
 })
---
+++ src/views/gameMaker.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="game-maker">
+    <canvas ref="canvas"></canvas>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      canvas: null,
+      ctx: null
+    }
+  },
+  mounted () {
+    this.canvas = this.$refs.canvas
+    this.canvas.width = document.body.clientWidth
+    this.canvas.height = document.body.clientHeight
+    this.ctx = this.canvas.getContext('2d')
+    this.initEvent()
+  },
+  methods: {
+    initEvent () {
+      // 监听鼠标移动事件
+      // this.canvas.onmousemove = (e) => {
+      //   // const { offsetX, offsetY }
+      // }
+      this.canvas.addEventListener("dragover", function(e) {
+        e.preventDefault();
+        e.stopPropagation()
+      });
+      this.canvas.addEventListener("dragenter", function(e) {
+        // this.canvas.classList.add("dragover");
+        e.preventDefault();
+        e.stopPropagation()
+        console.log('mouseup', e)
+      });
+      this.canvas.addEventListener("drop", function(e) {
+        e.preventDefault();
+        e.stopPropagation()
+
+        var data = e.dataTransfer.getData("image/png");
+        debugger
+        console.log(data);
+      });
+      // this.canvas.addEventListener('mousemove', (e) => {
+      //   console.log('mousemove', e)
+      //   e.preventDefault()
+      //   e.stopPropagation()
+      // })
+      // this.canvas.addEventListener('mouseup', (e) => {
+      //   console.log('mouseup', e)
+      //   e.preventDefault()
+      //   e.stopPropagation()
+      // })
+    },
+    createSprite () {
+
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.game-maker {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
